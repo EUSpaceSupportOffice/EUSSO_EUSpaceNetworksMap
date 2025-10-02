39af4e0dd3d64212302433cfd1e48d30fb9c3bf9
EUSpaceSupportOffice <167756950+EUSpaceSupportOffice@users.noreply.github.com>
2025-10-02 15:32:31 +0200

    Additions for STM and Galileo update
    added icon filed
    added Galileo and STM Ambassadors lists

---
+++ GalileoAmbassadors.js
@@ -0,0 +1,37 @@
+  const GalAmbs = [
+    {
+      "Name": "University of Michigan Space Institute",
+      "Address": "2455 Hayward St, 48109 Hayward St, United States",
+      "Coordinates": "42.29448036809762, -83.71150387393469",
+      "Contact": "Christopher Ruf",
+      "Email": "cruf@umich.edu",
+      "Link": "https://space.umich.edu/"
+    },
+    {
+      "Name": "Laboratoire d'Informatique Signal et Image de la Côte d'Opale (LISIC)",
+      "Address": "Maison de la Recherche Blaise Pascal - BP 719, 50 rue Ferdinand Buisson, 62228 Calais, France",
+      "Coordinates": "50.953506110040735, 1.880732082272241",
+      "Contact": "Serge Reboul",
+      "Email": "serge.reboul@univ-littoral.fr",
+      "Link": "https://lisic-prod.univ-littoral.fr/"
+    },
+    {
+      "Name": "CETRA BASE",
+      "Address": "C/ Deyanira 59C 28022 Madrid, Spain",
+      "Coordinates": "40.44297662390925, -3.5842999785092995",
+      "Contact": "Luis Arnaiz Mateo",
+      "Email": "luis.arnaiz@cetrabase.com",
+      "Link": "https://www.cetrabase.com/"
+    },
+    {
+      "Name": "Institute of Navigation, University of Stuttgart",
+      "Address": "Breitscheidstr. 2 70174 Stuttgart, Germany",
+      "Coordinates": "48.78062102332563, 9.171588543190863",
+      "Contact": "Thomas Hobiger",
+      "Email": "thomas.hobiger@ins.uni-stuttgart.de",
+      "Link": "https://www.ins.uni-stuttgart.de/en/"
+    }
+    ];
+
+    exports =GalAmbs;
+
---
+++ STMAmbassadors.js
@@ -0,0 +1,21 @@
+  const STMAmbs = [
+    {
+      "Name": "Arribes Enlightment",
+      "Address": "Calle Pablo Picasso, 19 28691 Villanueva de la Cañada, Spain",
+      "Coordinates": "40.440986594732976, -4.003329917761644",
+      "Contact": "Noelia Sanchez Ortiz",
+      "Email": "NOSO@ARRIB.ES",
+      "Link": "https://arrib.es/"
+    },
+    {
+      "Name": "Skypuzzler Aps",
+      "Address": "Ostergade 16, 4 DK-1100 Copernhagen, Denmark",
+      "Coordinates": "55.68017556924395, 12.58382939784428",
+      "Contact": "Elgiz Baskaya",
+      "Email": "elgiz.baskaya@skypuzzler.com",
+      "Link": "https://www.skypuzzler.com/"
+    }
+    ];
+
+    exports =STMAmbs;
+
