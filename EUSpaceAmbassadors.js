a5db1d5519fd58995015585c2dd98f6ae1e63268
EUSpaceSupportOffice <167756950+EUSpaceSupportOffice@users.noreply.github.com>
2025-12-10 23:25:23 +0100

    Add files via upload

---
+++ EUSpaceAmbassadors.js
@@ -0,0 +1,16 @@
+  const GalAmbs = [
+    {
+      "Name": "PROVA",
+      "Address": "provamockup",
+      "Coordinates": "45.2, 7.2",
+      "Contact": "Pr. Ova",
+      "Email": "prova@prova.prova",
+      "Link": "http://www.provaprovaprova.it"
+    },
+
+    ];
+
+    exports =GalAmbs;
+
+
+
