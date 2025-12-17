fa3c19c49ad7978a26cb2a3ac606f4e7d548cdd5
EUSpaceSupportOffice <167756950+EUSpaceSupportOffice@users.noreply.github.com>
2025-12-17 15:15:35 +0100

    Update EUSpaceAmbassadors.js
    Added Aerospace Valley, Pole Mer Bretagne Atlantique, Uni Luxembourg

--- EUSpaceAmbassadors.js
+++ EUSpaceAmbassadors.js
@@ -1,16 +1,32 @@
   const EUSAmbs = [
     {
-      "Name": "PROVA",
-      "Address": "provamockup",
-      "Coordinates": "45.2, 7.2",
-      "Contact": "Pr. Ova",
-      "Email": "prova@prova.prova",
-      "Link": "http://www.provaprovaprova.it"
+      "Name": "Aerospace Valley",
+      "Address": "3 Rue Tarfaya, 31400 Toulouse, France",
+      "Coordinates": "43.5636730101519, 1.488744655199879",
+      "Contact": "Dominique Cachenaut",
+      "Email": "cachenaut@aerospace-valley.com",
+      "Link": "www.aerospace-valley.com"
     },
-
+    {
+      "Name": "Pôle Mer Bretagne Atlantique",
+      "Address": "525, avenue Alexis de Rochon 29280 Plouzané, France",
+      "Coordinates": "48.357792220104734, -4.564087773425103",
+      "Contact": "Sophie Cordillet",
+      "Email": "sophie.cordillet@polemer-ba.com",
+      "Link": "https://www.pole-mer-bretagne-atlantique.com/en/booster-morespace"
+    },
+    {
+      "Name": "University of Luxembourg",
+      "Address": "2 Av. de l'Universite L, 4365 Esch-sur-Alzette Luxembourg",
+      "Coordinates": "49.50426654529269, 5.949402868955214",
+      "Contact": "Sajad Tabibi",
+      "Email": "sajad.tabibi@uni.lu",
+      "Link": "https://www.uni.lu/en/"
+    },
     ];
 
     exports =EUSAmbs;
+
 
 
 
